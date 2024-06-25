8e0115f637efb07a4c7b8e5866987375b5135bc2
argenisailo <lopezargenisai@gmail.com>
2024-06-25 16:33:32 -0600

    Update

--- armstrong_number.py
+++ armstrong_number.py
@@ -1,17 +1,36 @@
-def isArmstrong(num):
-    number = list(str(num))
+# Libraries approach
+def isArmstrongV1(number):
+    number_list = list(str(number))
+    result = 0
 
-    aux = 0
+    for i in range(len(number_list)):
+        result += pow(int(number_list[i]), len(number_list))
 
-    for i in range(len(number)):
-        aux += pow(int(number[i]), len(number))
+    if result != number:
+        return False
+    
+    return True
 
-    if aux != num:
+# Raw approach
+def isArmstrongV2(number):
+    number_string = str(number)
+    number_list = []
+    result = 0
+
+    # Slicing
+    for i in number_string[::-1]:
+        number_list.append(int(i))
+
+    for i in range(len(number_list)):
+        result += pow(int(number_list[i]), len(number_list))
+    
+    if result != number:
         return False
     
     return True
 
 if __name__ == "__main__":
-    number = 120
+    number = 153
 
-    print(isArmstrong(number))
+    print(isArmstrongV1(number))
+    print(isArmstrongV2(number))
