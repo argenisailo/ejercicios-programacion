8cfc9a7e8effcfaa51ba3a146804e004aec62de1
argenisailo <lopezargenisai@gmail.com>
2024-06-27 10:07:25 -0600

    Update palindrome_number.py

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,8 +1,9 @@
-def isPalindrome(number):
+# Libraries approach
+def isPalindromeV1(number):
     number_string = str(number)
     number_list = list(reversed(number_string))
-
     result = ''
+    
     for element in number_list:
         result += element
     
@@ -11,7 +12,21 @@ def isPalindrome(number):
     
     return True
 
+# Raw approach
+def isPalindromeV2(number):
+    number_string = str(number)
+    reversed_number = ''
+
+    for i in range(len(number_string), 0, -1):
+        reversed_number += number_string[i-1]
+
+    if reversed_number != number_string:
+        return False
+    
+    return True
+
 if __name__ == "__main__":
     number = 2002
 
-    print(isPalindrome(number))
+    print(isPalindromeV1(number))
+    print(isPalindromeV2(number))
